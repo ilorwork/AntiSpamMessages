403cdb2a415a319fda0a20bff834f7e0a4736248
ilorwork <ilorwork64@gmail.com>
2022-11-12 23:08:01 +0200

    initial commit

---
+++ bot.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_API;
+const bot = new TelegramBot(token, { polling: true });
+
+// bot.onText(/aa/i, (msg, match) => {
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, match[0]);
+//   bot.deleteMessage(chatId, msg.message_id);
+//   console.log(msg);
+// });
+
+bot.onText(/./, (msg) => {
+  const frases = ["סקס בישראל", "תשואה של עד 85%", "להשיג יותר מ-$100 ליום"];
+  const chatId = msg.chat.id;
+
+  const found = frases.find((frase) => msg.text.includes(frase));
+  if (!found) return;
+
+  bot.deleteMessage(chatId, msg.message_id);
+  bot.sendMessage(chatId, "Forbidden content deleted");
+});
