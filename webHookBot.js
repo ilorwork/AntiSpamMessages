2f2cd990362bfec17e6b7eac4e5e5da32013b3ce
ilorwork <ilorwork64@gmail.com>
2023-10-22 17:38:42 +0300

    Make the code more modular, by seperating msg handling to a seperate method, make the code async and create a json file for the phrases

--- webHookBot.js
+++ webHookBot.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const bodyParser = require("body-parser");
+const forbiddenPhrases = require("./phrases.json");
 const app = express();
 
 // Set up Express server
 const url = process.env.BASE_URL;
-const port = 3000; // You can choose any available port
+const port = process.env.PORT; // You can choose any available port
 
 const myChatId = process.env.MY_CHAT_ID;
 
@@ -17,11 +18,8 @@ const bot = new TelegramBot(token);
 // Set up your Webhook URL (publicly accessible server URL with a unique path)
 const webhookPath = process.env.WEBHOOK_PATH;
 const webhookURL = `${url}${webhookPath}`;
-// Render URL
-// const webhookURL = `https://antispammessages.onrender.com${webhookPath}`;
 
 // For testing on localhost with an open to the wide internet connection I use ngrok:
-// const webhookURL = `equally-included-zebra.ngrok-free.app${webhookPath}`;
 // my ngrok dashboard - https://dashboard.ngrok.com/cloud-edge/domains
 // Tunnel command:
 // ngrok http --domain=equally-included-zebra.ngrok-free.app 3000
@@ -32,49 +30,42 @@ bot.setWebHook(webhookURL);
 // Use body-parser to parse JSON requests
 app.use(bodyParser.json());
 
+// Define a function to handle incoming messages
+const handleIncomingMessage = async (msg) => {
+  console.log("################################# Incoming Message");
+  // This line causes an error, enable when debugging failure situation is needed.
+  // bot.sendMessage(myChatId, `Forbidden content`, msg.message.text);
+
+  const chatId = msg.message.chat.id;
+  console.log("chat id: ", chatId, "text: ", msg.message.text);
+
+  const found = forbiddenPhrases.find((phrase) =>
+    msg.message.text.includes(phrase)
+  );
+  console.log("Is forbidden content: ", !!found);
+  if (!found) return;
+
+  await bot.deleteMessage(chatId, msg.message.message_id);
+  await bot.sendMessage(chatId, "Forbidden content deleted");
+  console.log(
+    `Forbidden content deleted in chat: ${chatId}, content: ${msg.message.text}`
+  );
+  console.info("Full message object: ", msg);
+};
+
 // Create an endpoint for the Webhook
-app.post(webhookPath, (req, res) => {
+app.post(webhookPath, async (req, res) => {
   try {
     const msg = req.body;
-    // Handle incoming updates (messages) from Telegram here
-    // Your existing bot code for processing messages can go here
-    const frases = [
-      "𝒊𝒏𝒗еѕт",
-      "Making money online",
-      "making money online",
-      "Guys, write, let's talk)",
-      "Ребята, пишите, поговорим)",
-      "Hello boys:)",
-      "Shall we talk?)",
-      "Hi, everybody:)",
-      "Hi, I want to chat.",
-    ];
-
-    console.log("################################# test");
-    // This line causes an error, enable when debugging failure situation is needed.
-    // bot.sendMessage(myChatId, `Forbidden content`, msg.message.text);
-
-    const chatId = msg.message.chat.id;
-    console.log("chat id: ", chatId, "text: ", msg.message.text);
-
-    const found = frases.find((frase) => msg.message.text.includes(frase));
-    console.log("Is forbidden content: ", !!found);
-    if (!found) return;
-
-    bot.deleteMessage(chatId, msg.message.message_id);
-    bot.sendMessage(chatId, "Forbidden content deleted");
-    console.log(
-      `Forbidden content deleted in chat: ${chatId}, content: ${msg.message.text}`
-    );
-    console.info("Full message object: ", msg);
+    await handleIncomingMessage(msg);
     res.status(200).send("OK");
   } catch (error) {
-    // Handle the case where the received data is not a valid message or any other error
-    console.error("An error has occure", error);
-    bot.sendMessage(myChatId, `An error has occure ${error}`);
-    bot.sendMessage(
+    // Handle errors and notify the admin
+    console.error("An error has occurred", error);
+    await bot.sendMessage(myChatId, `An error has occurred: ${error}`);
+    await bot.sendMessage(
       myChatId,
-      `msg details ${JSON.stringify(req.body.message)}`
+      `Message details ${JSON.stringify(req.body.message)}`
     );
     res.status(200).send("OK");
   }
